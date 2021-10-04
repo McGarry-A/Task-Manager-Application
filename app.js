0b45abec6d3302dc93376b61ed6c3ff81aa57a79
Ahmed mcgarry <ahmedmcgarry@Ahmeds-Mac-mini.local>
2021-10-04 23:08:06 +0100

    drag and drop & restructure adding card

--- app.js
+++ app.js
@@ -21,12 +21,11 @@ class List {
         this.div = document.createElement('div')
         this.listHeading = document.createElement('h3');
         this.addCardButton = document.createElement('button');
-        this.addCardInput = document.createElement('input');
-
+        
         //append elements to parent
         this.toDoListElement.append(this.listHeading);
         this.toDoListElement.append(this.addCardButton);
-        this.toDoListElement.append(this.addCardInput);
+     
         this.toDoListElement.append(this.div);
 
         //set to do list title
@@ -38,23 +37,93 @@ class List {
 
         //event listener for creating a new card
         this.addCardButton.addEventListener('click', () => {
-            this.addCard();
-            this.addCardInput.value = '';
+            this.addCardMenu();
+        });
+
+        //Drag and Drop Event listeners
+        this.toDoListElement.addEventListener('dragover',(e)=>{
+            e.preventDefault();
+            const draggable = document.querySelector('.dragging')
+            this.toDoListElement.append(draggable)
+            this.toDoListElement.classList.add('toDoListHover')
+        })
+
+        this.toDoListElement.addEventListener('dragleave',(e)=>{
+            e.preventDefault();
+            this.toDoListElement.classList.remove('toDoListHover');
+        })
+
+        this.toDoListElement.addEventListener('drop',(e)=>{
+            this.toDoListElement.classList.remove('toDoListHover');
         })
     }
 
-    addCard(){
-        let text = this.addCardInput.value;
-        new Card(text, this.div);
+    addCard(name,description){
+        
+        new Card(name, description, this.div);
+    }
+
+    addCardMenu(){
+        
+        this.menuContainer = document.createElement('div');
+        
+        this.cardNameLabel = document.createElement('label')
+        this.cardName = document.createElement('input')
+        
+        this.taskDescriptionLabel = document.createElement('label')
+        this.taskDescription = document.createElement('textarea');
+        
+        this.cancel = document.createElement('button');
+        this.saveCard = document.createElement('button');
+        
+
+        document.body.appendChild(this.menuContainer);
+        this.menuContainer.appendChild(this.cardNameLabel);
+        this.menuContainer.appendChild(this.cardName);
+        this.menuContainer.appendChild(this.taskDescriptionLabel)
+        this.menuContainer.appendChild(this.taskDescription);
+        this.menuContainer.appendChild(this.cancel);
+        this.menuContainer.appendChild(this.saveCard);
+
+        this.cardNameLabel.innerText = 'Task Name'
+        this.taskDescriptionLabel.innerText = 'Task Description'
+        this.cancel.innerText = 'X';
+        this.saveCard.innerText = 'Save';
+        this.taskDescription.placeholder = 'Enter details of your task here...';
+
+        this.menuContainer.classList.add('menu');
+        this.cardName.classList.add('cardName');
+        
+        this.cardName.innerText = this.text;
+
+        this.cancel.addEventListener('click', ()=>{
+            this.menuContainer.remove();
+        })
+        
+        //Save button on the menu
+        this.saveCard.addEventListener('click', ()=>{
+            /*
+            we need to make the heading editbable, and save the changes to the card object.
+            we need to make the taskDescription editable, and save the changes to the card object.
+            */
+
+            let name = this.cardName.value
+            let description = this.taskDescription.value
+            this.addCard(name,description)
+            this.menuContainer.remove();
+        })
+
+        
     }
 }
 
 class Card {
 
-    constructor(text, place){
+    constructor(text, description, place){
 
         this.text = text;
         this.place = place;
+        this.description = description;
 
         this.render();
     }
@@ -64,14 +133,82 @@ class Card {
         //create card elements 
         this.card = document.createElement('div');
         this.label = document.createElement('label');
+        this.deleteButton = document.createElement('button');
+
         this.label.innerText = this.text;
 
         //add class lists
         this.card.classList.add('card')
+        this.card.draggable = 'true'
 
         //append elements to card
         this.card.append(this.label);
-        this.place.append(this.card)
+        this.place.append(this.card);
+        this.card.appendChild(this.deleteButton);
+
+        this.card.addEventListener('click',()=>{
+            this.renderMenu();
+        });
+
+
+        //Event listeners for dragging card & delete button
+        this.card.addEventListener('dragstart',(e)=>{
+            let draggedCard = e.target
+            draggedCard.classList.add('dragging');
+
+            setTimeout(()=>{
+                draggedCard.style.display = 'none';
+            }, 0)
+        })
+
+        this.card.addEventListener('dragend', (e)=> {
+            e.target.classList.remove('dragging');
+
+            setTimeout(()=>{
+                e.target.style.display = 'block'
+            },0);
+        });
+
+        this.deleteButton.addEventListener('click', (e)=> {
+            this.card.remove();
+        })
+    }
+
+    renderMenu(){
+        this.menuContainer = document.createElement('div');
+        this.menuHeading = document.createElement('h2');
+        this.removeMenu = document.createElement('button');
+        this.saveMenu = document.createElement('button');
+        this.taskDescription = document.createElement('textarea');
+
+        document.body.appendChild(this.menuContainer);
+        this.menuContainer.appendChild(this.menuHeading);
+        this.menuContainer.appendChild(this.removeMenu);
+        this.menuContainer.appendChild(this.saveMenu);
+        this.menuContainer.appendChild(this.taskDescription);
+
+        this.menuHeading.contentEditable = 'true';
+        this.removeMenu.innerText = 'X';
+        this.saveMenu.innerText = 'Save';
+        
+        this.menuContainer.classList.add('menu');
+        this.menuHeading.classList.add('menuheading');
+        
+        this.menuHeading.innerText = this.text;
+        this.taskDescription.value = this.description;
+
+        this.removeMenu.addEventListener('click', ()=>{
+            this.menuContainer.remove();
+        })
+        
+        //Save button on the menu
+        this.saveMenu.addEventListener('click', ()=>{
+            /*
+            we need to make the heading editbable, and save the changes to the card object.
+            we need to make the taskDescription editable, and save the changes to the card object.
+            */
+            this.menuContainer.remove();
+        })
     }
 }
 
